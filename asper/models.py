82292c609968cd6b5a9e717023ff2bdceb5ffa72
Anshuman Prajapati <44315336+anshumanprajapatiap@users.noreply.github.com>
2020-05-14 03:11:44 +0530

    First Commit
    Profile edit and Deployed Part2

---
+++ asper/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class UserDetail(models.Model):
+    usr = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    image = models.FileField(null=True)
+    phone = models.TextField(max_length=10, null=True)
+    dept = models.TextField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.usr.username
+
+class FirstTask(models.Model):
+    usr = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+
+    Q1 = models.TextField(max_length=100, null=True)
+    Q2 = models.TextField(max_length=100, null=True)
+    Q3 = models.TextField(max_length=100, null=True)
+    Q4 = models.TextField(max_length=100, null=True)
+    Q5 = models.TextField(max_length=100, null=True)
+    Q6 = models.TextField(max_length=100, null=True)
+    Q7 = models.TextField(max_length=100, null=True)
+    Q8 = models.TextField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.usr.username
---
+++ theaseperteam/urls.py
@@ -0,0 +1,36 @@
+"""theaseperteam URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from asper.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('test/', Test),
+    path('', Index, name='Index'),
+    path('team/', Team, name='Team'),
+    path('pillar/', Pillar, name='Pillar'),
+    path('profile/', Profile, name='Profile'),
+    path('signin/', SignIn, name='SignIn'),
+    path('signup/', SignUp, name='SignUp'),
+    path('logout/', Logout, name='logout'),
+    path('firsttask/', FirstTk, name='FirstTask'),
+    path('profiles/', AllProfile, name='AllProfiles'),
+
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
